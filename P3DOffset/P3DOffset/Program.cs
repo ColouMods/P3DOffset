489a2d627f0a975dc985b99843afc56c24437fb2
ColouMods <32771001+ColouMods@users.noreply.github.com>
2025-03-17 09:49:17 +0000

    Started basic functionality.
    Supports opening a file, applying an offset to Mesh chunks, and outputting a file.

---
+++ P3DOffset/P3DOffset/Program.cs
@@ -0,0 +1,27 @@
+﻿using NetP3DLib.P3D;
+using NetP3DLib.P3D.Chunks;
+using System.Numerics;
+
+string inputPath = args[0];
+string outputPath = "output.p3d";
+
+// Check file exists here
+
+P3DFile p3dFile = new(inputPath);
+
+// Get offsets here
+var offset = new Vector3(100, -5.5f, 200);
+
+foreach (var staticEntity in p3dFile.GetChunksOfType<NetP3DLib.P3D.Chunks.StaticEntityChunk>()) {
+	foreach (var mesh in staticEntity.GetChunksOfType<NetP3DLib.P3D.Chunks.MeshChunk>()) {
+		foreach (var oldPrimitiveGroup in mesh.GetChunksOfType<NetP3DLib.P3D.Chunks.OldPrimitiveGroupChunk>()) {
+			var positionList = oldPrimitiveGroup.GetFirstChunkOfType<NetP3DLib.P3D.Chunks.PositionListChunk>();
+
+			for (int i = 0; i < positionList.Positions.Count; i++) {
+				positionList.Positions[i] += offset;
+			}
+		}
+	}
+}
+
+p3dFile.Write(outputPath);
